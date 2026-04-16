90744b7c32c493eeccd4742db388d1be801729a3
kanon8888 <mdrakibhasankanon@gmail.com>
2026-04-16 21:13:08 +0600

    leven commit

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -12,7 +12,6 @@ const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [theme, setTheme] = useState("light");
 
-  
   useEffect(() => {
     const auth = document.cookie.includes("auth=true");
     setIsLoggedIn(auth);
@@ -48,14 +47,13 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100 px-4">
-      
+    <div className="navbar bg-white px-4 shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-5 w-5 text-black"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -68,24 +66,18 @@ const Navbar = () => {
               />
             </svg>
           </div>
-          <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow">
+          <ul className="menu menu-sm dropdown-content bg-white rounded-box mt-3 w-52 p-2 shadow">
             {nav}
           </ul>
         </div>
         <Logo />
       </div>
 
-      
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{nav}</ul>
+        <ul className="menu menu-horizontal px-1 text-black">{nav}</ul>
       </div>
 
-      
       <div className="navbar-end flex gap-2">
-        
-       
-
-        
         {!isLoggedIn ? (
           <Link href="/login">
             <button className="btn btn-primary btn-outline">Login</button>
